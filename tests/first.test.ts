6c4525fcd271cac41e68296d89134c9c71872375
luanws <luan.w.silveira@gmail.com>
2021-02-24 18:06:36 -0300

    Create first.test.ts

---
+++ tests/first.test.ts
@@ -0,0 +1,10 @@
+import 'ts-jest'
+
+describe('Soma', () => {
+    it('Somar dois números', () => {
+        expect(2 + 2).toBe(4)
+    })
+    it('Somar três números', () => {
+        expect(2 + 2 + 3).toBe(7)
+    })
+})
